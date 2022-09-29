e0108760cee249de5b0438637abbd10b85ce9517
JasonPaulino <jasonp0830@gmail.com>
2022-09-29 19:31:31 +0000

    Jason finished question 10

--- index.js
+++ index.js
@@ -70,4 +70,16 @@ function betweenTwentyAndForty(num) {
 }
 console.log(betweenTwentyAndForty(50))
 
+const largest = (num1, num2, num3) => {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 > num1 && num2 > num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+
+console.log(largest(20, 100, 15));
+
 
